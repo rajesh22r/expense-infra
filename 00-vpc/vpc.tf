b8c36e1a2ac36fdbc44836da7406628f51341782
rajesh22r <rajesh.raju22698@gmail.com>
2025-06-13 17:43:34 +0530

    jenkins

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -7,4 +7,5 @@ module "vpc" {
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
   is_peering_required   = var.is_peering_required
+  vpc_cidr = var.vpc_cidr
 }
