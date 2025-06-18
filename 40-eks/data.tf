9ea12eaf0ec4f2a75c9c39ee8c0ad7fed9591d20
rajesh22r <rajesh.raju22698@gmail.com>
2025-06-18 13:17:44 +0530

    jenkins

--- 40-eks/data.tf
+++ 40-eks/data.tf
@@ -13,6 +13,10 @@ data "aws_ssm_parameter" "control_plane_sg_id" {
   name = "/${var.project_name}/${var.environment}/control_plane_sg_id"
 }
 
+data "aws_ssm_parameter" "node_sg_id" {
+  name = "/${var.project_name}/${var.environment}/node_sg_id"
+}
+
 data "aws_ami" "joindevops" {
   
   most_recent      = true
